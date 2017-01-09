bee52a036509274c5d2f5aface141a2eedcbd558
Christopher Wells <cwellsny@nycap.rr.com>
2017-01-09 01:06:06 -0500

    Change script to use argparse
    Change the script so that it uses argparse instead of manually parsing the cli arguments.

--- upsilonextract/__main__.py
+++ upsilonextract/__main__.py
@@ -3,18 +3,25 @@
 from __future__ import print_function
 import upsilonextract as up
 import numpy as np
+
+import argparse
 import sys
 
-def main():
-    if len(sys.argv) != 6:
-        sys.exit("Invalid Arguments.")
+program_description = "UPSILoN Extract is a Python script for extracting information from (time, magnitude, error) light curve data. It was created from the functionality built into the UPSILoN periodic variable star classifier (Kim & Bailer-Jones 2015)."
 
-    prefix = sys.argv[1]
-    suffix = sys.argv[2]
-    padding = sys.argv[3]
-    start = int(sys.argv[4])
-    end = int(sys.argv[5])
+def main():
+    parser = argparse.ArgumentParser(description=program_description)
+    parser.add_argument('prefix', type=str, help='part of the path of the data files in front of the id number')
+    parser.add_argument('suffix', type=str, help='part of the path of the data files after the id number')
+    parser.add_argument('padding', type=int, help='number of digits to pad the id numbers to')
+    parser.add_argument('start', type=int, help='id number of first file to process')
+    parser.add_argument('end', type=int, help='id number of last file to process')
+
+    args = parser.parse_args()
+
+    process_input_files(args.prefix, args.suffix, args.padding, args.start, args.end)
     
+def process_input_files(prefix, suffix, padding, start, end):
     for x in range(start, end + 1):
         number = ("%0" + str(padding) + "d") % (x,)
         file_path = prefix + number + suffix
